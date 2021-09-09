40de99ebc402028379320e71c770b8727d6dbeb7
MojiburRahaman <santo68683@gmail.com>
2021-09-09 13:11:31 +0600

    db

--- data.php
+++ data.php
@@ -1,3 +1,3 @@
 <?php 
-$data = mysqli_connect('ec2-35-174-56-18.compute-1.amazonaws.com', 'ikcjfqqfhbqjmm', '47c35db34a238e9f1ba071341229be60beb673f5a8f41f2c56123fea69265eb8', 'd1rkj4hjqpi6t8d1rkj4hjqpi6t8',5432 );
+$data = mysqli_connect('ec2-35-174-56-18.compute-1.amazonaws.com', 'ikcjfqqfhbqjmm', '47c35db34a238e9f1ba071341229be60beb673f5a8f41f2c56123fea69265eb8', 'd1rkj4hjqpi6t8d1rkj4hjqpi6t8','5432' );
 ?>
